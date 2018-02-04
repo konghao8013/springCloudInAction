8a889ad54fd0d48f44e6cc77ba010175658a0a3c
konghao <konghao8013@163.com>
2018-02-04 19:27:57 +0800

    增加 spring config

---
+++ e-book/config/config-client/src/main/java/com/konghao/book/config/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.konghao.book.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@Value("${book.config}")
+	private String msg;
+
+	@RequestMapping("/test")
+	public String test() {
+		return msg;
+	}
+}
